ae4d60f4c1bce9218755b708215c47d8fa8d9e85
PapaLoupSkies <rocher2d2@gmail.com>
2021-05-17 00:51:26 +0200

    ngnix conf

---
+++ init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get update
+
+sudo apt install docker.io
+
+sudo systemctl start docker
+sudo systemctl enable docker
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
