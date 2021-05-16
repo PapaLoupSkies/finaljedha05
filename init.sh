24702d82982eb8f99b457963d3819908782eabf7
PapaLoupSkies <rocher2d2@gmail.com>
2021-05-17 00:53:48 +0200

    ngnix conf

--- init.sh
+++ init.sh
@@ -9,3 +9,7 @@ sudo systemctl enable docker
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+cd ~/server
+sudo docker-compose build
+sudo docker-compose up
